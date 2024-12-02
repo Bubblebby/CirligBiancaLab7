6052ebbeb3b5f2e78c18545574d14c9e2cacf0f2
Bubblebby <geanina.cirlig@stud.ubbcluj.ro>
2024-12-02 10:26:43 +0200

    lab 8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace CirligBiancaLab7
+﻿using System;
+using CirligBiancaLab7.Data;
+using System.IO;
+namespace CirligBiancaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
+            
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
